e55f9378ac3cc463ab22e0e5a16d7f60882d58b5
NuevoInversor <runnertheworld@gmail.com>
2026-09-01 16:40:44 +0200

    Centralize visible Nexus version display

--- workout-controls-v212.js
+++ workout-controls-v212.js
@@ -94,11 +94,7 @@
     renderWorkout=wrapped;
   }
 
-  function updateVersion(){
-    const v=document.querySelector('header .version');
-    if(v) v.textContent=`Training - ${VERSION} (${STAMP})`;
-    document.title=`Nexus Training ${VERSION}`;
-  }
+  function updateVersion(){ /* Version centralizada en polar-intelligence-v222.js */ }
 
   function boot(){
     ensureStyle();
