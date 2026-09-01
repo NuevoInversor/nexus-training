f47e7ac55cd073086d5db109760de1399bfc2d7b
NuevoInversor <runnertheworld@gmail.com>
2026-09-01 16:40:49 +0200

    Centralize visible Nexus version display

--- mesocycle-david-v213.js
+++ mesocycle-david-v213.js
@@ -205,11 +205,7 @@
     document.getElementById('nexusUpcomingMesocycle')?.remove();
   }
 
-  function updateVersion(){
-    const v=document.querySelector('header .version');
-    if(v) v.textContent=`Training - ${VERSION} (${STAMP})`;
-    document.title=`Nexus Training ${VERSION}`;
-  }
+  function updateVersion(){ /* Version centralizada en polar-intelligence-v222.js */ }
 
   function tick(){
     patchTargets();
