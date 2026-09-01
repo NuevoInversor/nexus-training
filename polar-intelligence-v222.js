797080e4d5336d86792e28f113f37ff8176f0d38
NuevoInversor <runnertheworld@gmail.com>
2026-09-01 10:14:41 +0200

    Fix Polar sync UI version v2.23

--- polar-intelligence-v222.js
+++ polar-intelligence-v222.js
@@ -1,6 +1,6 @@
 (() => {
-  const VERSION='v2.22';
-  const STAMP='01/09/2026 09:54:08';
+  const VERSION='v2.23';
+  const STAMP='01/09/2026 10:16:00';
   const CFG=window.NEXUS_CLOUD||{};
   const BASE=(CFG.url||'').replace(/\/$/,'')+'/functions/v1';
   const BOOT_KEY='nexus_polar_v222_bootstrap';
