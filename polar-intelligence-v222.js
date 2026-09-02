3e124af3a8e017faf8b16a498987c063be639554
NuevoInversor <runnertheworld@gmail.com>
2026-09-02 13:27:05 +0200

    Bump Nexus to v2.36

--- polar-intelligence-v222.js
+++ polar-intelligence-v222.js
@@ -1,6 +1,6 @@
 (() => {
-  const VERSION='v2.35';
-  const STAMP='02/09/2026 12:43:00';
+  const VERSION='v2.36';
+  const STAMP='02/09/2026 13:30:00';
   const CFG=window.NEXUS_CLOUD||{};
   const BASE=(CFG.url||'').replace(/\/$/,'')+'/functions/v1';
   const BOOT_KEY='nexus_polar_v222_bootstrap';
