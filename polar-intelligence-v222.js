c59ec9d2d73ae611dee0ef7827cf71cff1c6ab65
NuevoInversor <runnertheworld@gmail.com>
2026-09-02 11:30:11 +0200

    Set centralized Nexus version v2.34

--- polar-intelligence-v222.js
+++ polar-intelligence-v222.js
@@ -1,6 +1,6 @@
 (() => {
-  const VERSION='v2.33';
-  const STAMP='02/09/2026 11:28:00';
+  const VERSION='v2.34';
+  const STAMP='02/09/2026 11:32:00';
   const CFG=window.NEXUS_CLOUD||{};
   const BASE=(CFG.url||'').replace(/\/$/,'')+'/functions/v1';
   const BOOT_KEY='nexus_polar_v222_bootstrap';
